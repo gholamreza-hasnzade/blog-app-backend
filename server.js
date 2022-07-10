84a812b55273b4be5fbfdf15a94cb28f777d381a
gholamreza-hasnzade <gholamreza15402@gmail.com>
2022-07-10 19:21:02 +0430

    start project

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const dotenv = require("dotenv")
+const dbConnect = require("./config/db/dbConnect")
+
+dotenv.config();
+const app = express()
+
+//DB
+dbConnect() 
+
+// server
+const PORT = process.env.PORT || 5000
+app.listen(PORT , console.log(`server running ${PORT}`))
